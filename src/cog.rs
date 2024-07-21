402cc4491079636c20754ff8bffb6a48931f3e4f
Anthony Lukach <anthonylukach@gmail.com>
2024-07-21 12:45:04 -0700

    Working towards successfully parsing tags

--- src/cog.rs
+++ src/cog.rs
@@ -1,8 +1,13 @@
 use crate::errors::CogErr;
 use byteorder::{BigEndian, ByteOrder, LittleEndian, ReadBytesExt};
+use geotiff::{
+    geotiff::decode_tag,
+    lowlevel::{tag_size, TIFFTag, TagValue},
+};
 use http_range_client::BufferedHttpRangeClient;
 use serde::{Deserialize, Serialize};
 use std::io::{Error, ErrorKind, Read};
+use tiff::tags::Type;
 use worker as cf;
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -33,7 +38,7 @@ impl Cog {
             };
             ifds.push(ifd);
 
-            if (next_ifd_offset as usize) == 0 {
+            if next_ifd_offset == 0 {
                 cf::console_debug!("No more IFDs");
                 break;
             }
@@ -115,7 +120,7 @@ impl CogHeader {
 #[derive(Debug, Deserialize, Serialize)]
 pub struct IFD {
     pub count: u16,
-    pub entries: Vec<IFDEntry>,
+    // pub entries: Vec<IFDEntry>,
 }
 
 impl IFD {
@@ -140,77 +145,173 @@ impl IFD {
         cf::console_log!("Num fields: {:?}", entry_count);
 
         // a sequence of 12-byte field entries
-        let mut entries: Vec<IFDEntry> = Vec::with_capacity(entry_count as usize);
+        // let mut entries: Vec<IFDEntry> = Vec::with_capacity(entry_count as usize);
         for entry_num in 0..entry_count as usize {
-            let fields_bytes = client
+            let mut fields_bytes = client
                 // Take our IFD offset, add 2 bytes for the entry count, and then add 12 bytes for each entry we've processed so far
                 .get_range(offset + 2 + (entry_num * 12), 12 as usize)
                 .await?;
 
-            let ifd_entry = IFDEntry::parse::<T>(fields_bytes)?;
+            // Each 12-byte IFD Entry is in the following format.
+            //
+            // Bytes  | Description
+            // ---------------------
+            // 0-1	  | The Tag that identifies the field
+            // 2-3	  | The field type
+            // 4-7	  | Count of the indicated type
+            // 8-11	  | The Value Offset, the file offset (in bytes) of the Value for the field. The Value is
+            //        | expected to begin on a word boundary; the correspond-ing Value Offset will thus be an
+            //        | even number. This file offset may point anywhere in the file, even after the image data.
+            //
+            // A TIFF field is a logical entity consisting of TIFF tag and its value. This logical concept
+            // is implemented as an IFD Entry, plus the actual value if it doesn’t fit into the value/offset
+            // part, the last 4 bytes of the IFD Entry. The terms TIFF field and IFD entry are interchangeable
+            // in most contexts.
 
-            entries.push(ifd_entry);
+            // TODO: Use `tiff` or `geotiff` to parse tag data
+
+            // Bytes 0..1: u16 tag ID
+            let tag_value = fields_bytes.read_u16::<T>()?;
+            let Some(tag) = decode_tag(tag_value) else {
+                // https://www.loc.gov/preservation/digital/formats/content/tiff_tags.shtml
+                cf::console_warn!(
+                    "#{:?}/{:?}: Ignoring entry with unexpected tag value ({:?})",
+                    entry_num + 1,
+                    entry_count,
+                    tag_value
+                );
+                continue;
+                // TODO: Fail on bad tag type
+                // return Err(CogErr::from(Error::new(
+                //     ErrorKind::InvalidData,
+                //     format!("Invalid tag {:04X}", tag_value),
+                // )));
+            };
+
+            // Bytes 2..3: u16 field Type
+            let field_type_value = fields_bytes.read_u16::<T>()?;
+            let Some(field_type) = Type::from_u16(field_type_value) else {
+                cf::console_warn!(
+                    "#{:?}/{:?}: Ignoring entry with unexpected field type value ({:?})",
+                    entry_num + 1,
+                    entry_count,
+                    field_type_value
+                );
+                continue;
+                // TODO: Fail on bad field type
+                // return Err(CogErr::from(Error::new(
+                //     ErrorKind::InvalidData,
+                //     format!("Invalid tag type {:04X}", field_type_value),
+                // )));
+            };
+            let value_size = tag_size(&field_type);
+
+            // Bytes 4..7: u32 number of Values of type
+            let num_values = fields_bytes.read_u32::<T>()?;
+            let tot_size = num_values * value_size;
+
+            // Let's get the value(s) of this tag.
+            let mut values = Vec::with_capacity(num_values as usize);
+
+            // Bytes 8..11: u32 offset in file to Value
+            let value_offset = fields_bytes.read_u32::<T>()?;
+            let mut value_data: &[u8] = match tot_size <= 4 {
+                true => {
+                    // NOTE: If the value is <= 4 bytes, the value offset is the value itself. I can't
+                    // find this mentioned in the spec, but all reference implementations do this.
+                    // cf::console_debug!(
+                    //     "Total size of {:?} (<=4), treating value offset as value",
+                    //     tot_size,
+                    // );
+                    let mut buf = [0u8; 4];
+                    T::write_u32(&mut buf, value_offset);
+                    &buf.to_vec()
+                }
+                false => {
+                    client
+                        .get_range((value_offset + tot_size) as usize, value_size as usize)
+                        .await?
+                }
+            };
+            for _ in 0..num_values as usize {
+                let mut buf = [0u8; 4];
+                value_data.read(&mut buf)?;
+                let val = Self::vec_to_tag_value::<T>(buf.to_vec(), &field_type)?;
+                values.push(val);
+            }
+
+            cf::console_debug!(
+                "#{:?}/{:?}: {:?}, {:?}",
+                entry_num + 1,
+                entry_count,
+                tag,
+                values
+            );
+
+            // let tag = ifd_entry.as_tag::<T>(client).await?;
+            // cf::console_log!("Tag: {:?}", tag);
+
+            // entries.push(ifd_entry);
         }
+
+        // cf::console_log!("IFD entries: {:?}", entries);
 
         // a 4-byte offset of the next IFD (or 0 if none)
         let mut next_ifd_offset_reader = client
             .get_range(offset + 2 + (entry_count as usize * 12), 4)
             .await?;
-        let next_ifd_offset = next_ifd_offset_reader
+        let next_ifd_offset: u32 = next_ifd_offset_reader
             .read_u32::<T>()
             .expect("slice with incorrect length");
 
         Ok((
             Self {
                 count: entry_count,
-                entries,
+                // entries,
             },
             next_ifd_offset,
         ))
     }
-}
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct IFDEntry {
-    tag: u16,
-    field_type: u16,
-    num_values: u32,
-    value_offset: u32,
-}
-
-impl IFDEntry {
-    /**
-     * Each 12-byte IFD Entry is in the following format.
-     *
-     * Bytes  | Description
-     * ---------------------
-     * 0-1	  | The Tag that identifies the field
-     * 2-3	  | The field type
-     * 4-7	  | Count of the indicated type
-     * 8-11	  | The Value Offset, the file offset (in bytes) of the Value for the field. The Value is
-     *        | expected to begin on a word boundary; the correspond-ing Value Offset will thus be an
-     *        | even number. This file offset may point anywhere in the file, even after the image data.
-     *
-     * A TIFF field is a logical entity consisting of TIFF tag and its value. This logical concept
-     * is implemented as an IFD Entry, plus the actual value if it doesn’t fit into the value/offset
-     * part, the last 4 bytes of the IFD Entry. The terms TIFF field and IFD entry are interchangeable
-     * in most contexts.
-     */
-    fn parse<T: ByteOrder>(mut reader: impl Read) -> Result<Self, Error> {
-        // TODO: Use `tiff` or `geotiff` to parse tag data
-        let tag = reader.read_u16::<T>()?;
-        // 2-byte field type
-        let field_type = reader.read_u16::<T>()?;
-        // 4-byte count of the number of values
-        let num_values = reader.read_u32::<T>()?;
-        // 4-byte value offset
-        let value_offset = reader.read_u32::<T>()?;
-        Ok(Self {
-            tag,
-            field_type,
-            num_values,
-            value_offset,
-        })
+    /// Converts a Vec<u8> into a TagValue, depending on the type of the tag. In the TIFF file
+    /// format, each tag type indicates which value it stores (e.g., a byte, ascii, or long value).
+    /// This means that the tag values have to be read taking the tag type into consideration.
+    fn vec_to_tag_value<Endian: ByteOrder>(vec: Vec<u8>, tpe: &Type) -> Result<TagValue, CogErr> {
+        let len = vec.len();
+        match tpe {
+            Type::BYTE => Ok(TagValue::ByteValue(vec[0])),
+            Type::ASCII => Ok(TagValue::AsciiValue(
+                String::from_utf8_lossy(&vec).to_string(),
+            )),
+            Type::SHORT => Ok(TagValue::ShortValue(Endian::read_u16(&vec[..]))),
+            Type::LONG => Ok(TagValue::LongValue(Endian::read_u32(&vec[..]))),
+            Type::RATIONAL => {
+                cf::console_debug!("Parsing {:?} ({:?}). This will likely fail.", vec, tpe);
+                Ok(TagValue::RationalValue((
+                    // TODO: This fails...
+                    Endian::read_u32(&vec[..(len / 2)]),
+                    Endian::read_u32(&vec[(len / 2)..]),
+                )))
+            }
+            Type::SBYTE => Ok(TagValue::SignedByteValue(vec[0] as i8)),
+            Type::UNDEFINED => Ok(TagValue::ByteValue(0)),
+            Type::SSHORT => Ok(TagValue::SignedShortValue(Endian::read_i16(&vec[..]))),
+            Type::SLONG => Ok(TagValue::SignedLongValue(Endian::read_i32(&vec[..]))),
+            Type::SRATIONAL => Ok(TagValue::SignedRationalValue((
+                Endian::read_i32(&vec[..(len / 2)]),
+                Endian::read_i32(&vec[(len / 2)..]),
+            ))),
+            Type::FLOAT => Ok(TagValue::FloatValue(Endian::read_f32(&vec[..]))),
+            Type::DOUBLE => Ok(TagValue::DoubleValue(Endian::read_f64(&vec[..]))),
+            // Type::IFD => ,
+            // Type::LONG8 => ,
+            // Type::SLONG8 => ,
+            // Type::IFD8 => ,
+            _ => Err(CogErr::from(Error::new(
+                ErrorKind::InvalidData,
+                format!("Invalid field type {:?}", tpe),
+            ))),
+        }
     }
 }
 
@@ -218,4 +319,12 @@ impl IFDEntry {
 enum TIFFByteOrder {
     LittleEndian,
     BigEndian,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Tag {
+    // tag_value: u16,
+    // field_type_value: u16,
+    // num_values: u32,
+    // value_offset: u32,
 }
